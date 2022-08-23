372f6525dc68fa17c17912fb1b2a3433a8fee0ef
namlh <hoainamle023@gmail.com>
2022-08-23 22:45:31 +0700

    Create grid blocks

--- tailwind.config.js
+++ tailwind.config.js
@@ -7,6 +7,10 @@ module.exports = {
         cream: "#FFF7F0",
         black: "#030303",
         brown: "#7A746E",
+        galacticBlue: "#755CDE",
+        summerYellow: "#F6A560",
+        lightRed: "#EB7565",
+        darkPurple: "#552049",
       },
       fontFamily: {
         poppins: ["Poppins"],
@@ -14,6 +18,15 @@ module.exports = {
       fontSize: {},
       backgroundImage: {
         logo: "url('./src/assets/logo.svg')",
+        graphicDesign: "url('/src/assets/pattern-graphic-design.svg')",
+        uiUx: "url('/src/assets/pattern-ui-ux.svg')",
+        apps: "url('/src/assets/pattern-apps.svg')",
+        illustrations: "url('/src/assets/pattern-illustrations.svg')",
+        photo: "url('/src/assets/pattern-photography.svg')",
+        motion: "url('/src/assets/pattern-motion-graphics.svg')",
+      },
+      backgroundPosition: {
+        "top-right-block": "right 1rem top 1rem",
       },
     },
   },
